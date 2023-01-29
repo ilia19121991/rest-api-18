7ce8583f48f171b717765ccd9a3db3d81326a3cb
ilia1 <ilia19011991@gmail.com>
2023-01-29 23:33:14 +0300

    added ReqresInHomeworkTests

---
+++ src/test/java/ReqresInHomeworkTests.java
@@ -0,0 +1,81 @@
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static io.restassured.http.ContentType.JSON;
+import static org.hamcrest.Matchers.*;
+
+public class ReqresInHomeworkTests {
+
+    @Test
+    void checkUserExistsTest() {
+        given()
+                .log().uri()
+                .when()
+                .get("https://reqres.in/api/users?page=2")
+                .then()
+                .log().status()
+                .log().body()
+                .body("page", is(2))
+                .body("data.email", hasItem("rachel.howell@reqres.in"));
+
+    }
+
+    @Test
+    void deleteUsersTest() {
+        given()
+                .log().uri()
+                .when()
+                .delete("https://reqres.in/api/users/2")
+                .then()
+                .log().status()
+                .statusCode(204);
+    }
+
+    @Test
+    void getSingleUserTest() {
+        given()
+                .log().uri()
+                .when()
+                .get("https://reqres.in/api/users/2")
+                .then()
+                .log().status()
+                .log().body()
+                .body("data.id", is(2))
+                .body("data.email", is("janet.weaver@reqres.in"));
+
+    }
+
+    @Test
+    void createUserTest() {
+        String user = "{ \"name\": \"morpheus\", \"job\": \"leader\" }";
+
+        given()
+                .log().uri()
+                .contentType(JSON)
+                .body(user)
+                .when()
+                .post("https://reqres.in/api/users")
+                .then()
+                .log().body()
+                .log().status()
+                .statusCode(201)
+                .body("name", is("morpheus"), "job", is("leader"));
+    }
+
+    @Test
+    void updateUserTest() {
+        String updateUser = "{ \"name\": \"Andrey\", \"job\": \"QA\" }";
+
+        given()
+                .log().uri()
+                .contentType(JSON)
+                .body(updateUser)
+                .when()
+                .put("https://reqres.in/api/users/2")
+                .then()
+                .log().status()
+                .log().body()
+                .statusCode(200)
+                .body("name", is("Andrey"), "job", is("QA"));
+    }
+}
--- src/test/java/ReqresInTests.java
+++ src/test/java/ReqresInTests.java
@@ -1,4 +1,3 @@
-import io.restassured.http.ContentType;
 import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.given;
@@ -7,28 +6,28 @@ import static org.hamcrest.Matchers.is;
 
 public class ReqresInTests {
 
-     /*
-    1. Make POST request to https://reqres.in/api/login
-    with body { "email": "eve.holt@reqres.in", "password": "cityslicka" }
-    2. Get response { "token": "QpwL5tke4Pnpja7X4" }
-    3. Check "token" is "QpwL5tke4Pnpja7X4"
-     */
-     @Test
-     void loginTest() {
-         String data = "{ \"email\": \"eve.holt@reqres.in\", \"password\": \"cityslicka\" }";
+    /*
+   1. Make POST request to https://reqres.in/api/login
+   with body { "email": "eve.holt@reqres.in", "password": "cityslicka" }
+   2. Get response { "token": "QpwL5tke4Pnpja7X4" }
+   3. Check "token" is "QpwL5tke4Pnpja7X4"
+    */
+    @Test
+    void loginTest() {
+        String data = "{ \"email\": \"eve.holt@reqres.in\", \"password\": \"cityslicka\" }";
 
-         given()
-                 .log().uri()
-                 .contentType(JSON)
-                 .body(data)
-                 .when()
-                 .post("https://reqres.in/api/login")
-                 .then()
-                 .log().status()
-                 .log().body()
-                 .statusCode(200)
-                 .body("token", is("QpwL5tke4Pnpja7X4"));
-     }
+        given()
+                .log().uri()
+                .contentType(JSON)
+                .body(data)
+                .when()
+                .post("https://reqres.in/api/login")
+                .then()
+                .log().status()
+                .log().body()
+                .statusCode(200)
+                .body("token", is("QpwL5tke4Pnpja7X4"));
+    }
 
     @Test
     void unSupportedMediaTypeTest() {
@@ -55,9 +54,8 @@ public class ReqresInTests {
                 .log().status()
                 .log().body()
                 .statusCode(400)
-                .body("error", is ("Missing email or username"));
+                .body("error", is("Missing email or username"));
     }
-
 
 
     @Test
@@ -74,7 +72,7 @@ public class ReqresInTests {
                 .log().status()
                 .log().body()
                 .statusCode(400)
-                .body("error", is ("Missing password"));
+                .body("error", is("Missing password"));
     }
 
 }
