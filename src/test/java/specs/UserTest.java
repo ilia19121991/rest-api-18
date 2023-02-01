bfc002bd78d5b9a1215268b80ebc7db5766768fa
ilia1 <ilia19011991@gmail.com>
2023-02-01 19:29:14 +0300

    added LombokUserData

--- src/test/java/specs/UserTest.java
+++ src/test/java/specs/UserTest.java
@@ -1,8 +1,12 @@
 package specs;
 
+import lombok.LombokUserData;
+import models.User;
+import models.UserData;
 import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static specs.Specs.request;
 import static specs.Specs.resronseSpec;
 
@@ -17,6 +21,39 @@ public class UserTest {
                 .then()
                 .spec(resronseSpec)
                 .log().body();
-        // @formatter:ob
+        // @formatter:on
     }
+
+    @Test
+    void singleUserWithModel() {
+        // @formatter:off
+       UserData data = given()
+                .spec(request)
+                .when()
+                .get("/users/2")
+                .then()
+                .spec(resronseSpec)
+                .log().body()
+                .extract().as(UserData.class);
+        // @formatter:on
+        assertEquals(2, data.getData().getId());
+    }
+
+    @Test
+    void singleUserWithLombokModel() {
+
+        LombokUserData data = given()
+                .spec(request)
+                .when()
+                .get("/users/2")
+                .then()
+                .spec(resronseSpec)
+                .log().body()
+                .extract().as(LombokUserData.class);
+        // @formatter:on
+        assertEquals(2, data.getUser().getId());
+        // @formatter:off
+    }
+
+
 }
