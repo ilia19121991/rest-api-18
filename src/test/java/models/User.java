760a3867ab353e8f9af2ee2f4b618139eb8ec943
ilia1 <ilia19011991@gmail.com>
2023-02-01 18:27:13 +0300

    added User and UserData

--- src/test/java/models/User.java
+++ src/test/java/models/User.java
@@ -29,4 +29,35 @@ public class User {
     @JsonProperty("last_name")
     private String lastName;
 
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
 }
